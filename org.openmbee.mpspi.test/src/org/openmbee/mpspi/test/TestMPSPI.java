fc0d9b6526da36ef316e5f775ac9556e8c709262
Hisashi Miyashita <himi@maplesoft.com>
2019-02-18 02:10:31 -0500

    org.openmbee.mpspi.test.TestMPSPI.testGet(): Added

--- org.openmbee.mpspi.test/src/org/openmbee/mpspi/test/TestMPSPI.java
+++ org.openmbee.mpspi.test/src/org/openmbee/mpspi/test/TestMPSPI.java
@@ -1,7 +1,13 @@
 package org.openmbee.mpspi.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openmbee.mpspi.Activator;
@@ -16,10 +22,27 @@ public class TestMPSPI {
 		discovery = Activator.getDefault().newDiscovery();
 	}
 
-	@Test
-	public void test() throws Exception {
-		MPAdapter adapter = discovery.newAdapter(null, null, null, null);
+    private MPAdapter open() throws Exception {
+		MPAdapter adapter = discovery.newAdapter(null, EcorePackage.eNS_URI, null, null);
 		assertNotNull(adapter);
+		URI uri = URI.createURI("platform:/plugin/org.eclipse.emf.ecore/model/Ecore.ecore");
+		adapter.load(uri, null);
+        return adapter;
+    }
+
+
+	@Test
+	public void testOpenEcore() throws Exception {
+        open();
 	}
+
+	@Test
+    public void testGet() throws Exception {
+        MPAdapter adapter = open();
+        EObject root = adapter.getRoots().get(0);
+        assertTrue(root instanceof EPackage);
+        String name = (String) adapter.get(root, EcorePackage.eINSTANCE.getENamedElement_Name());
+        assertEquals(name, "ecore");
+    }
 
 }
