cff0b48d4435ea053070449f0032922d60de92cb
Hisashi Miyashita <himi@maplesoft.com>
2019-03-16 00:02:28 -0400

    Added serialVersionUID

--- org.openmbee.mpspi/src/org/openmbee/mpspi/exceptions/MPDataLoadException.java
+++ org.openmbee.mpspi/src/org/openmbee/mpspi/exceptions/MPDataLoadException.java
@@ -2,6 +2,11 @@ package org.openmbee.mpspi.exceptions;
 
 public class MPDataLoadException extends MPFailureException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6601807011984294895L;
+
 	public MPDataLoadException(String message, Throwable cause) {
 		super(message, cause);
 	}
